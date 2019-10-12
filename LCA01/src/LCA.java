db4b66c2946f282dd3a7081803fb71a65265947d
Ellen Whelan <whelane5@tcd.ie>
2019-10-12 10:40:25 +0100

    Added some build m ethods fortrees and tests

--- LCA01/src/LCA.java
+++ LCA01/src/LCA.java
@@ -31,40 +31,10 @@ public class LCA {
         }
 
     public static void main(String[] args) {
-        // Creating a binary tree
-           /* Node rootNode = createBinaryTree();
-            System.out.println("Lowest common ancestor for node 5 and 30:");
-            Node node5 = new Node(5);
-            Node node30 = new Node(30);
-            System.out.println(lowestCommonAncestor(rootNode, node5, node30).data);*/
+
 
     }
 
-    public static Node createBinaryTree() {
 
-        Node rootNode = new Node(40);
-        Node node20 = new Node(20);
-        Node node10 = new Node(10);
-        Node node30 = new Node(30);
-        Node node60 = new Node(60);
-        Node node50 = new Node(50);
-        Node node70 = new Node(70);
-        Node node5 = new Node(5);
-        Node node45 = new Node(45);
-        Node node55 = new Node(55);
-
-        rootNode.left = node20;
-        rootNode.right = node60;
-
-        node20.left = node10;
-        node20.right = node30;
-
-        node60.left = node50;
-        node60.right = node70;
-
-        node10.left = node5;
-        node50.right = node55;
-        return rootNode;
-    }
 
 }
--- LCA01/src/LCATest.java
+++ LCA01/src/LCATest.java
@@ -26,4 +26,46 @@ public class LCATest {
 
     }
 
+    @Test
+    public void testBuildTree(){
+        LCA.Node rootNode = createBinaryTree();
+        assertEquals(40, rootNode.data);
+    }
+    @Test
+    public void testBasicCase(){
+        LCA.Node rootNode = createBinaryTree();
+        System.out.println("Lowest common ancestor for node 5 and 30:");
+        LCA.Node node5 = new LCA.Node(5);
+        LCA.Node node30 = new LCA.Node(30);
+        assertEquals(20, LCA.lowestCommonAncestor(rootNode, node5, node30).data);
+    }
+
+
+    public static LCA.Node createBinaryTree() {
+
+        LCA.Node rootNode = new LCA.Node(40);
+        LCA.Node node20 = new LCA.Node(20);
+        LCA.Node node10 = new LCA.Node(10);
+        LCA.Node node30 = new LCA.Node(30);
+        LCA.Node node60 = new LCA.Node(60);
+        LCA.Node node50 = new LCA.Node(50);
+        LCA.Node node70 = new LCA.Node(70);
+        LCA.Node node5 = new LCA.Node(5);
+        LCA.Node node45 = new LCA.Node(45);
+        LCA.Node node55 = new LCA.Node(55);
+
+        rootNode.left = node20;
+        rootNode.right = node60;
+
+        node20.left = node10;
+        node20.right = node30;
+
+        node60.left = node50;
+        node60.right = node70;
+
+        node10.left = node5;
+        node50.right = node55;
+        return rootNode;
+    }
+
 }
