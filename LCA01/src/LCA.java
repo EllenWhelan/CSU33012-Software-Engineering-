7baa57ec27eee4ddc365660520582a643fe24713
Ellen Whelan <whelane5@tcd.ie>
2019-10-15 14:27:30 +0100

    Added LCA DAG method

--- LCA01/src/LCA.java
+++ LCA01/src/LCA.java
@@ -35,6 +35,45 @@ public class LCA {
 
     }*/
 
+   //DAG classs stuff
+    public static class DAGNode {
+        int data;
+        DAGNode[] parents, children;
+
+        DAGNode(int value) {
+            data = value;
+            parents = children = null;
+        }
+    }
+
+    public DAGNode dagFindLCA(DAGNode root, DAGNode a, DAGNode b){
+        if (root==null) return null;
+        if (root == a ||root == b) return root;
+        if(a==b)        return a;
+
+        ArrayList <DAGNode> lca = new ArrayList<DAGNode>();
+        for(int i=0; i<a.parents.length;i++){
+            for(int j=0;j<b.parents.length;j++){
+                if(a.parents[i].data==b.parents[j].data){
+                    lca.add(a.parents[i]);
+                }
+            }
+
+        }
+
+        if(lca==null){
+            if (a.data>b.data) lca.add(dagFindLCA(root,a.parents[0], b));
+            else lca.add(dagFindLCA(root,a,b.parents[0]));
+        }
+
+        DAGNode maxTemp = lca.get(0);
+        for (int i=0; i<lca.size();i++){
+            if(lca.get(i).data > maxTemp.data) maxTemp= lca.get(i);
+        }
+        return maxTemp;
+    }
+
+
 
 
 }
